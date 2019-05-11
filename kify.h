85ad63bd9cd2d6d3a47e6523fc37d759e863315f
Leo Cornelius <48571127+LeoCornelius@users.noreply.github.com>
2019-05-11 22:14:40 +0100

    Create kify.h

---
+++ kify.h
@@ -0,0 +1,4 @@
+// Copywrite 2019 The Kegcoin Gold devs (rmove this and ill consume you)
+
+#define required_nodes_percent 50 // How much of the netnodes need to agree with the node for it to acept the  chain (dont change)
+#define required_hash_percent 50 // How much of the nethash a chain fork requires (dont change)
