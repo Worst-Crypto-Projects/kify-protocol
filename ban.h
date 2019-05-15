dcc8c2f2c0eee05068e3d7d5bcf9cdf7dd0cd038
Leo Cornelius <48571127+LeoCornelius@users.noreply.github.com>
2019-05-15 17:13:20 +0100

    add ban types (needs optimising
    TODO: change from if statements to a array

--- ban.h
+++ ban.h
@@ -1,6 +1,25 @@
 
-const string types = {
-"Permanant",
-"Temporary,
-"Soft"
+const string types = { //Types of bans
+"Permanant", // P
+"Temporary,  // T
+"Soft",      // S
+}
+const uint64_t codes
+  "0", // P
+  "1", // T
+  "2", // S
+}
+string getType(code) {
+  if (code !in codes) {
+    return "invalid Ban code";
+  }elif (code == 0) {
+    return "P";
+  }elif (code == 1) {
+    return "T";
+  }elif (code == 2) {
+    return "S";
+  }else
+    std::cout << "wtf has happened??"; // i THINK this should never happen (unless you add ban types and foget to add a if statment) becuase the first if statment should catch nubers that are not in the ban type list
+   exit();
+  }
 }
