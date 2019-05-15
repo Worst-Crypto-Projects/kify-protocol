a9cf8ec066f4be40b7a70249a5006ad23ecb15c8
Leo Cornelius <48571127+LeoCornelius@users.noreply.github.com>
2019-05-15 17:34:57 +0100

    Update kify.h

--- kify.h
+++ kify.h
@@ -2,3 +2,4 @@
 
 const string max_peer_polls peerCount() // Max amount of peers to poll forblock height (set to peerCount() to poll every conected peer)
 const uint64_t poll_timeout 100 // Time (in secconds) before we drop the conection to peer when reciving no data
+const uint64_t coin_rpc_port 4057
