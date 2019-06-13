c75bc0356f37a974e5bb39de337de6a88cf762ee
Leo Cornelius <kegcoingold@gmail.com>
2019-06-14 00:01:19 +0100

    Update ban.cpp

--- ban.cpp
+++ ban.cpp
@@ -1,12 +1,18 @@
 #include <ban.h>
 #include <iostream>
 void main() {
-  cout << "Ban server online";
+  std::cout << "Ban server online";
   bool banServer = True;
   return 0;
 }
-void banPeer(char* ip, string reason) {
-  peerList.remove(ip);
-  blackList.add(ip);
-  cout << "Banned peer " + ip + " for " + reason;
+bool banPeer(char* ip, string reason) {
+  if (ip in getPeerList()) {
+    peerList.remove(ip);
+    blackList.add(ip);
+    std::cout << "Banned peer " + ip + " for " + reason;
+    return true;
+  } else {
+    std::cout << "ERROR: cannot ban peer, not in peerlist!";
+    return false;
+  }
 }
